dd68f810ba019b257cd57435594017cc33578d2f
Mykyta <112129014+Nikita10Polyakov@users.noreply.github.com>
2023-11-29 16:38:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
-import asyncio
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types.web_app_info import WebAppInfo
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+#TOKEN
+TOKEN = ""
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
 
 
-logging.basicConfig(level=logging.INFO)
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Відкрити сторінку магазину", web_app=WebAppInfo(url="https://"))) #Сайт СЮДА!!!
+    await message.answer("Вітаю у боті! Натисни на кнопку, щоб перейти до магазину", reply_markup=markup)
 
-bot = Bot(token="6563322621:AAHv9zDSPU5JBikI73DG1ks6o123123123")    #ТОКЕН СЮДА!!!
-
-dp = Dispatcher()
-
-
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    builder = InlineKeyboardBuilder()
-    builder.row(types.InlineKeyboardButton(
-        text="Відкрити сторінку магазину", web_app=WebAppInfo(url="https://123123123.com"))    #САЙТ СЮДА!!!
-    )
-
-    await message.answer(
-        'Вітаю у боті! Натисни на кнопку, щоб перейти до магазину',
-        reply_markup=builder.as_markup()
-    )
-
-
-async def main():
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+executor.start_polling(dp)
