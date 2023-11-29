ac7ee6cef2f44bbd8b11b9376d515938ad5853d5
Mykyta <112129014+Nikita10Polyakov@users.noreply.github.com>
2023-11-29 16:39:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Відкрити сторінку магазину", web_app=WebAppInfo(url="https://"))) #Сайт СЮДА!!!
+    markup.add(types.InlineKeyboardButton("Відкрити сторінку магазину", web_app=WebAppInfo(url="https://"))) #Сайт СЮДИ!!!
     await message.answer("Вітаю у боті! Натисни на кнопку, щоб перейти до магазину", reply_markup=markup)
 
 executor.start_polling(dp)
